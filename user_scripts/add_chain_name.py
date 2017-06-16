7b6596829156f87d5ae5e7b024be15652d45ddae
Victor Zhao <vzhao23@gmail.com>
2017-06-15 20:17:28 -0400

    script that adds chain name to pdb without

--- user_scripts/add_chain_name.py
+++ user_scripts/add_chain_name.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+"""
+add_chain_name.py
+
+The way MCPU prints PDB files, the chain name field is omitted.
+
+My run_MCPU.sh script has an awk command that depends on that
+field being present.
+
+This script modifies the input structure file in place by default
+
+"""
+
 
 import argparse
 import string

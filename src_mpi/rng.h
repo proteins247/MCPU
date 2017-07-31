97932d5aadfaa8df88a6d58394271205c0707f62
Victor Zhao <vzhao23@gmail.com>
2017-07-31 13:28:15 -0400

    elaborated comment. indentation. switched to u01fixedpt_closed_open_64_53

--- src_mpi/rng.h
+++ src_mpi/rng.h
@@ -6,7 +6,7 @@
  * using an algorithm from the Random123 library.
  *
  * Initialize by calling set_threefry_array with a user-supplied
- * seed (2^32 - 1 possible seeds). 
+ * seed (2^64 - 1 possible seeds). 
  *
  * For multi-processing situations, the user-supplied seed should
  * be different for each process if one seeks to avoid having
@@ -17,32 +17,32 @@
  * variables.
  *
  */
-static threefry4x64_ctr_t ctr={{}};
-static threefry4x64_key_t key={{}};
+static threefry4x64_ctr_t ctr = {{}};
+static threefry4x64_key_t key = {{}};
 
 void increment_counter() {
-  static size_t index = 0;
-  if (index == 4) {
-    index = 0;
-  }
-  ctr.v[index++]++;
+    static size_t index = 0;
+    if (index == 4) {
+        index = 0;
+    }
+    ctr.v[index++]++;
 }
 
-void set_threefry_array(unsigned int user_key) {
-  key.v[0] = user_key;
-  key.v[1] = user_key;
-  key.v[2] = user_key;
-  key.v[3] = user_key;
+void set_threefry_array(unsigned long int user_key) {
+    key.v[0] = user_key;
+    key.v[1] = user_key;
+    key.v[2] = user_key;
+    key.v[3] = user_key;
 }
 
 double threefryrand() {
-  static size_t randomNumberIndex = 4;
-  static threefry4x64_ctr_t result={{}};
-  if (randomNumberIndex == 4) {
-    result = threefry4x64(ctr, key);
-    randomNumberIndex = 0;
-    increment_counter();
-  }
-  return u01fixedpt_closed_closed_64_53(result.v[randomNumberIndex++]);
+    static size_t randomNumberIndex = 4;
+    static threefry4x64_ctr_t result = {{}};
+    if (randomNumberIndex == 4) {
+        result = threefry4x64(ctr, key);
+        randomNumberIndex = 0;
+        increment_counter();
+    }
+    return u01fixedpt_closed_open_64_53(result.v[randomNumberIndex++]);
 }
 

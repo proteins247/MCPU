c4ce5e7f1b2bf96f65f3a7af90495666cbcbec79
Victor Zhao <vzhao23@gmail.com>
2017-07-31 12:40:54 -0400

    rng.h updated so that modulo operator not needed. in general one shouldn't optimize perf like this though

--- src_mpi/rng.h
+++ src_mpi/rng.h
@@ -2,23 +2,30 @@
 #include "Random123/u01fixedpt.h"
 
 /* 
-threefryrand() generates random doubles in the range [0.0, 1.0]
-using an algorithm from the Random123 library.
-
-Initialize by calling set_threefry_array with a user-supplied
-seed. 
-
-For multi-processing situations, the user-supplied seed should
-be different for each thread if one seeks to avoid having
-the same stream of random numbers for each thread/process.
-
+ * threefryrand() generates random doubles in the range [0.0, 1.0]
+ * using an algorithm from the Random123 library.
+ *
+ * Initialize by calling set_threefry_array with a user-supplied
+ * seed (2^32 - 1 possible seeds). 
+ *
+ * For multi-processing situations, the user-supplied seed should
+ * be different for each process if one seeks to avoid having
+ * the same stream of random numbers for each thread/process.
+ *
+ * I'm unsure about the behavior of these functions in a
+ * multi-threaded situation where there is only one instance of these
+ * variables.
+ *
  */
 static threefry4x64_ctr_t ctr={{}};
 static threefry4x64_key_t key={{}};
 
 void increment_counter() {
   static size_t index = 0;
-  ctr.v[index++ % 4]++;
+  if (index == 4) {
+    index = 0;
+  }
+  ctr.v[index++]++;
 }
 
 void set_threefry_array(unsigned int user_key) {
@@ -31,9 +38,9 @@ void set_threefry_array(unsigned int user_key) {
 double threefryrand() {
   static size_t randomNumberIndex = 4;
   static threefry4x64_ctr_t result={{}};
-  if (!(randomNumberIndex % 4)) {
+  if (randomNumberIndex == 4) {
     result = threefry4x64(ctr, key);
-    randomNumberIndex -= 4;
+    randomNumberIndex = 0;
     increment_counter();
   }
   return u01fixedpt_closed_closed_64_53(result.v[randomNumberIndex++]);

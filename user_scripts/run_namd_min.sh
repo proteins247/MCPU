87a2c2b394e0214e5b60129a943afe721d68e958
Victor Zhao <vzhao23@gmail.com>
2020-04-27 23:13:28 -0400

    Make `run_namd_min.sh` work with current CentOS6
    NAMD modules on the cluster don't seem to load for me. Instead, it's really easy to download and untar NAMD. The script simply uses `namd2` located wherever NAMD was untar'ed to. Furthermore, some of the sed commands needed updating. Maybe with the updating of namd2 version, the way file locations are specified changed. I found that every path needs to be relative to wherever the .conf file is located.

--- user_scripts/run_namd_min.sh
+++ user_scripts/run_namd_min.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 
-# ./run_namd_min.sh structure_input_directory output_directory
-# Minimization for multiple structures in one directory
-# single core
+# run_namd_min.sh 
+# Minimization for multiple structures in one directory. single core
 
 echo run_namd_min.sh; echo
 
-. new-modules.sh
-module add legacy/0.0.1-fasrc01
-module add hpc/namd-2.9
+if [ $# -ne 2 ]; then
+    echo './run_namd_min.sh structure_input_directory output_directory'
+    exit 1
+fi
+
+# module purge
+# # module load centos6/0.0.1-fasrc01  ncf/1.0.0-fasrc01
+# # module load hpc/namd-2.9
+# module add GCC/7.3.0-2.30 OpenMPI/3.1.1
+# module add NAMD/2.13-mpi
+
 
 input_directory=$1
 output_directory=$2
-template=/n/home00/vzhao/opt/MCPU/user_scripts/namd_minimization.conf.template
+: ${template:=/n/home00/vzhao/opt/MCPU/user_scripts/namd_minimization.conf.template}
+: ${NAMDPATH:="/n/home00/vzhao/opt/NAMD_2.13_Linux-x86_64-multicore"}
+
+# SLURM associated
+: ${PARTITION:="shakhnovich,shakgpu,shared"}
+: ${TIME:=360}
+: ${DRYRUN:=}
 
 if [ ! -d "${output_directory}" ]; then
-    echo "Output directory does not exist, exiting"
-    exit 10
+    mkdir -p "${output_directory}"
 fi
 
 for pdb in ${input_directory}/*pdb; do
     echo "Working on ${pdb}"
     fileroot=$(basename ${pdb} .pdb)
-    coordinates=${input_directory}/${fileroot}.pdb
-    structure=${input_directory}/${fileroot}.psf
+    coordinates="${input_directory}/${fileroot}.pdb"
+    structure="${input_directory}/${fileroot}.psf"
     
     sed \
-	-e s:XXXINPSF:"${structure}": \
-	-e s:XXXINPDB:"${coordinates}": \
-	-e s:XXXOUTROOT:"${output_directory}/${fileroot}": \
+	-e s:XXXINPSF:"../${structure}": \
+	-e s:XXXINPDB:"../${coordinates}": \
+	-e s:XXXOUTROOT:"${fileroot}": \
 	< "$template" \
 	> "${output_directory}/namd_minimization_${fileroot}.conf"
 
-    sbatch -p shakgpu -n 1 -t 360 -o ${output_directory}/${fileroot}.log \
-    	   --wrap "namd2 ${output_directory}/namd_minimization_${fileroot}.conf"
+    $([ -n "$DRYRUN" ] && echo echo) \
+    	sbatch -p ${PARTITION} -n 1 -t ${TIME} --mem 512 \
+    	-o ${output_directory}/${fileroot}.slurm -J "min_${fileroot}" \
+    	--wrap "\"${NAMDPATH}/namd2\" ${output_directory}/namd_minimization_${fileroot}.conf"
+    # "${NAMDPATH}/namd2" ${output_directory}/namd_minimization_${fileroot}.conf
+    sleep 0.5
 done
